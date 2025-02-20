e594b45879cef939b587565fc6e61cff8655e8ad
wyattsteere <wyattsteere@gmail.com>
2025-02-19 19:38:00 -0800

    added data + grouped by year

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const width = 900 - margin.left - margin.right;
 const height = 400 - margin.top - margin.bottom;
 
 // Create SVG containers for both charts
-const svg1_RENAME = d3.select("#lineChart1") // If you change this ID, you must change it in index.html too
+const svgLine = d3.select("#lineChart1") // If you change this ID, you must change it in index.html too
     .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
@@ -22,7 +22,38 @@ const svg2_RENAME = d3.select("#lineChart2")
 // const tooltip = ...
 
 // 2.a: LOAD...
-d3.csv("YOUR_CSV_NAME.csv").then(data => {
+d3.csv("events.csv").then(data => {
+    console.log(data); 
+    console.log(typeof data[11])
+
+    // data.forEach(d => {
+    //     console.log(d["ev_year"]);       // Convert year to a number
+    //        // Rename column for clarity
+    // });
+
+    
+    const groupedByYear = d3.rollup(
+        data.filter(d => d.ev_year >= 1982),
+    
+        v => ({
+        inj_total_f: d3.sum(v, d => d.inj_tot_f),
+        inj_total_m: d3.sum(v, d => d.inj_tot_m),
+        inj_total_s: d3.sum(v, d => d.inj_tot_s)
+        }),
+        
+        d => d.ev_year
+    );
+
+    const yearlyData = Array.from(groupedByYear, ([ev_year, values]) => ({
+        ev_year,
+        ...values
+    }));
+    
+    yearlyData.sort((a, b) => a.ev_year - b.ev_year);
+
+    console.log(yearlyData)
+
+    
     // 2.b: ... AND TRANSFORM DATA
 
     // 3.a: SET SCALES FOR CHART 1
